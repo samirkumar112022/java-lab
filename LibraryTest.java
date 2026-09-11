46b2b616b7d6929b40762a0daaa64d75aa58fa7e
samirkumar112022 <shelbysamir1@gmail.com>
2026-09-11 12:08:55 +0530

    Add files via upload

---
+++ LibraryTest.java
@@ -0,0 +1,41 @@
+public class LibraryTest {
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics", "J. Author", 350.0, "ISBN001");
+        Book b2 = new Book("OOP Concepts", "K. Writer", 420.0, "ISBN002");
+ 
+        System.out.println(b1.getTitle() + " by " + b1.getAuthor());
+        System.out.println("Price: " + b2.getPrice());
+        System.out.println("Library: " + Book.libraryName);
+        System.out.println("Total books added: " + Book.bookCount);
+    }
+}
+
+class Book {
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+
+    static int bookCount = 0;
+    static final String libraryName = "Sunrise Public Library";
+
+    public Book(String title, String author, double price, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+        bookCount++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
